40938e326dafc7dc3b2efe92f8e64508060db134
jacqxu00 <jackiestuy@gmail.com>
2017-12-26 17:57:13 -0500

    added display function

---
+++ battleship.c
@@ -0,0 +1,21 @@
+void display {
+
+    printf("YOUR BOARD:                OPPONENT BOARD:\n\n");
+    printf("  A B C D E F G H I J        A B C D E F G H I J\n");
+    
+    int i; 
+    for (i = 0; i < 10; i++) {
+    
+        printf("%d ", i); //print row number
+        // print i-th line of txt file
+        printf("\n");
+    
+    }
+
+}
+
+int main() {
+
+    
+
+}
