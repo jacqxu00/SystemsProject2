4e6a7cc34c16aa4bccc420ad3aa4967b33da4f7d
FuruiFuyunoTaiyou <wchen10@stuy.edu>
2017-12-28 19:55:00 -0500

    changed struct names to char

--- battleship.c
+++ battleship.c
@@ -1,60 +1,60 @@
 #include "battleship.h"
 
 struct aircraft {
-	char name[256];
-	int length;
-	char coor[256];
+  char name;
+  int length;
+  char coor[256];
 };
 struct battleship {
-	char name[256];
-	int length;
-	char coor[256];
+  char name;
+  int length;
+  char coor[256];
 };
 struct cruiser {
-	char name[256];
-	int length;
-	char coor[256];
+  char name;
+  int length;
+  char coor[256];
 };
 struct submarine {
-	char name[256];
-	int length;
-	char coor[256];
+  char name;
+  int length;
+  char coor[256];
 };
 struct destroyer {
-	char name[256];
-	int length;
-	char coor[256];
+  char name;
+  int length;
+  char coor[256];
 };
 
-struct aircraft *create_aircraft (char *name, int size) {
-	struct aircraft *A = (struct aircraft *)malloc(sizeof(struct aircraft));
-	strcpy(A->name, name);
-	A->length = size;
-	return A;
+struct aircraft *create_aircraft (char name, int size) {
+  struct aircraft *A = (struct aircraft *)malloc(sizeof(struct aircraft));
+  A->name = name;
+  A->length = size;
+  return A;
 }
-struct battleship *create_battleship (char *name, int size) {
-	struct battleship *B = (struct battleship *)malloc(sizeof(struct battleship));
-	strcpy(B->name, name);
-	B->length = size;
-	return B;
+struct battleship *create_battleship (char name, int size) {
+  struct battleship *B = (struct battleship *)malloc(sizeof(struct battleship));
+  B->name = name;
+  B->length = size;
+  return B;
 }
-struct cruiser *create_cruiser (char *name, int size) {
-	struct cruiser *C = (struct cruiser *)malloc(sizeof(struct cruiser));
-	strcpy(C->name, name);
-	C->length = size;
-	return C;
+struct cruiser *create_cruiser (char name, int size) {
+  struct cruiser *C = (struct cruiser *)malloc(sizeof(struct cruiser));
+  C->name = name;
+  C->length = size;
+  return C;
 }
-struct submarine *create_submarine (char *name, int size) {
-	struct submarine *S = (struct submarine *)malloc(sizeof(struct submarine));
-	strcpy(S->name, name);
-	S->length = size;
-	return S;
+struct submarine *create_submarine (char name, int size) {
+  struct submarine *S = (struct submarine *)malloc(sizeof(struct submarine));
+  S->name = name;
+  S->length = size;
+  return S;
 }
-struct destroyer *create_destroyer (char *name, int size) {
-	struct destroyer *D = (struct destroyer *)malloc(sizeof(struct destroyer));
-	strcpy(D->name, name);
-	D->length = size;
-	return D;
+struct destroyer *create_destroyer (char name, int size) {
+  struct destroyer *D = (struct destroyer *)malloc(sizeof(struct destroyer));
+  D->name = name;
+  D->length = size;
+  return D;
 }
 
 void initialize_board(char board[10][10]) {
@@ -78,35 +78,41 @@ void print_board(char board[10][10]) {
 
 void display(char h_board[10][10], char o_board[10][10]) {
 
-    printf("\nHOME TERRITORY:\n");
-    printf("  A B C D E F G H I J\n");
-    print_board(h_board);
+  printf("\nHOME TERRITORY:\n");
+  printf("  A B C D E F G H I J\n");
+  print_board(h_board);
 
-    printf("\nOPPONENT TERRITORY:\n");
-    printf("  A B C D E F G H I J\n");
-    print_board(o_board);
+  printf("\nOPPONENT TERRITORY:\n");
+  printf("  A B C D E F G H I J\n");
+  print_board(o_board);
 
 }
 
 int main() {
 
-	struct aircraft *A = create_aircraft("A", 5);
-	struct battleship *B = create_battleship("B", 4);
-	struct cruiser *C = create_cruiser("C", 3);
-	struct submarine *S = create_submarine("S", 3);
-	struct destroyer *D = create_destroyer("D", 2);
+  //setup
+
+  struct aircraft *A = create_aircraft('A', 5);
+  struct battleship *B = create_battleship('B', 4);
+  struct cruiser *C = create_cruiser('C', 3);
+  struct submarine *S = create_submarine('S', 3);
+  struct destroyer *D = create_destroyer('D', 2);
 
   char home[10][10];
   char opponent[10][10];
   initialize_board(home);
   initialize_board(opponent);
 
-	printf("Aircraft: %s, %d\n", A->name, A->length);
-	printf("Battleship: %s, %d\n", B->name, B->length);
-	printf("Cruiser: %s, %d\n", C->name, C->length);
-	printf("Submarine: %s, %d\n", S->name, S->length);
-	printf("Destroyer: %s, %d\n", D->name, D->length);
+  printf("Aircraft: %c, %d\n", A->name, A->length);
+  printf("Battleship: %c, %d\n", B->name, B->length);
+  printf("Cruiser: %c, %d\n", C->name, C->length);
+  printf("Submarine: %c, %d\n", S->name, S->length);
+  printf("Destroyer: %c, %d\n", D->name, D->length);
 
-	display(home, opponent);
+  display(home, opponent);
+
+  //receive input
+  printf("Setup Phase: \nTo place a ship, enter the letter representing the ship, the letter representing the column, the digit representing the row, and the letter representing the direction, each separated by a space \nLeft: L \nRight: R \nUp: U \nDown: D \n\t For example: S A 0 D will place a submarine occupying A,0 A,1 A,2 \n");
+  
 
 }
