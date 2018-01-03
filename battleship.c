49d2e5fe55fe7ae979acb7a3490a5eafb02c0d19
Jacqueline Xu <jackiestuy@gmail.com>
2018-01-03 11:13:19 -0500

    done w encrypt_ship

--- battleship.c
+++ battleship.c
@@ -1,62 +1,5 @@
 #include "battleship.h"
 
-struct aircraft {
-  char name;
-  int length;
-  char coor[256];
-};
-struct battleship {
-  char name;
-  int length;
-  char coor[256];
-};
-struct cruiser {
-  char name;
-  int length;
-  char coor[256];
-};
-struct submarine {
-  char name;
-  int length;
-  char coor[256];
-};
-struct destroyer {
-  char name;
-  int length;
-  char coor[256];
-};
-
-struct aircraft *create_aircraft (char name, int size) {
-  struct aircraft *A = (struct aircraft *)malloc(sizeof(struct aircraft));
-  A->name = name;
-  A->length = size;
-  return A;
-}
-struct battleship *create_battleship (char name, int size) {
-  struct battleship *B = (struct battleship *)malloc(sizeof(struct battleship));
-  B->name = name;
-  B->length = size;
-  return B;
-}
-struct cruiser *create_cruiser (char name, int size) {
-  struct cruiser *C = (struct cruiser *)malloc(sizeof(struct cruiser));
-  C->name = name;
-  C->length = size;
-  return C;
-}
-struct submarine *create_submarine (char name, int size) {
-  struct submarine *S = (struct submarine *)malloc(sizeof(struct submarine));
-  S->name = name;
-  S->length = size;
-  return S;
-}
-struct destroyer *create_destroyer (char name, int size) {
-  struct destroyer *D = (struct destroyer *)malloc(sizeof(struct destroyer));
-  D->name = name;
-  D->length = size;
-  return D;
-}
-
 void initialize_board(char board[10][10]) {
   int i;
   for (i = 0; i < 10; i++) {
@@ -73,6 +16,18 @@ void print_board(char board[10][10]) {
       printf("%c ", board[r][c]);
     }
     printf("\n");
+  }
+}
+
+void encrypt_board(char board[10][10]) {
+  int r;
+  for (r = 0; r < 10; r++) {
+    for (c = 0; c < 10; c++) {
+      char c = board[r][c] - 'A';
+      if (c >= 0 && c <= 25) {
+        board[r][c] = '.';
+      }
+    }
   }
 }
 
@@ -171,7 +126,6 @@ void place_ship(char ship, char col, int row, char dir, char board[10][10]){
 int main() {
 
   //setup
-
   struct aircraft *A = create_aircraft('A', 5);
   struct battleship *B = create_battleship('B', 4);
   struct cruiser *C = create_cruiser('C', 3);
--- battleship.h
+++ battleship.h
@@ -2,3 +2,63 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+//ship structs
+struct aircraft {
+  char name;
+  int length;
+  char coor[256];
+};
+struct battleship {
+  char name;
+  int length;
+  char coor[256];
+};
+struct cruiser {
+  char name;
+  int length;
+  char coor[256];
+};
+struct submarine {
+  char name;
+  int length;
+  char coor[256];
+};
+struct destroyer {
+  char name;
+  int length;
+  char coor[256];
+};
+
+struct aircraft *create_aircraft (char name, int size) {
+  struct aircraft *A = (struct aircraft *)malloc(sizeof(struct aircraft));
+  A->name = name;
+  A->length = size;
+  return A;
+}
+struct battleship *create_battleship (char name, int size) {
+  struct battleship *B = (struct battleship *)malloc(sizeof(struct battleship));
+  B->name = name;
+  B->length = size;
+  return B;
+}
+struct cruiser *create_cruiser (char name, int size) {
+  struct cruiser *C = (struct cruiser *)malloc(sizeof(struct cruiser));
+  C->name = name;
+  C->length = size;
+  return C;
+}
+struct submarine *create_submarine (char name, int size) {
+  struct submarine *S = (struct submarine *)malloc(sizeof(struct submarine));
+  S->name = name;
+  S->length = size;
+  return S;
+}
+struct destroyer *create_destroyer (char name, int size) {
+  struct destroyer *D = (struct destroyer *)malloc(sizeof(struct destroyer));
+  D->name = name;
+  D->length = size;
+  return D;
+}
+
+//function headers
