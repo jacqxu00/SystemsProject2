dab1434ef7a7141fed279a8bbfc5dba0fc344905
Yedoh Kang <ykang@stuy.edu>
2018-01-25 15:52:12 -0500

    Update networking_setup.c

--- networking_setup.c
+++ networking_setup.c
@@ -3,7 +3,8 @@
 // code credit to Mr. DW
 // altered and added a few lines so that it would work with our code
 
-void error_check (int i, char *s) { //modify behavior after testing
+void error_check (int i, char *s) { 
+  // modify behavior after testing
   if (i < 0) {
     printf("[%s] error %d: %s\n", s, errno, strerror(errno));
     exit(1);
@@ -13,35 +14,35 @@ void error_check (int i, char *s) { //modify behavior after testing
 int server_setup (char * address) {
   int sd, i;
 
-  //create the socket
+  // create the socket
   sd = socket(AF_INET, SOCK_STREAM, 0);
   error_check(sd, "server socket");
   printf("[server] socket created\n");
 
-  //allow reuse of address
+  // allow reuse of address
   int enable = 1;
   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
   setsockopt(sd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int));
 
-  //setup structs for getaddrinfo
+  // setup structs for getaddrinfo
   struct addrinfo * hints, * results;
   hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET;  //IPv4 address
-  hints->ai_socktype = SOCK_STREAM;  //TCP socket
-  hints->ai_flags = AI_PASSIVE;  //Use all valid addresses
-  getaddrinfo(address, PORT, hints, &results); //NULL means use local address
+  hints->ai_family = AF_INET;  // IPv4 address
+  hints->ai_socktype = SOCK_STREAM;  // TCP socket
+  hints->ai_flags = AI_PASSIVE;  // Use all valid addresses
+  getaddrinfo(address, PORT, hints, &results); // NULL means use local address
 
-  //bind the socket to address and port
+  // bind the socket to address and port
   i = bind(sd, results->ai_addr, results->ai_addrlen);
   error_check(i, "server bind");
   printf("[server] socket bound\n");
 
-  //set socket to listen state
+  // set socket to listen state
   i = listen(sd, 10);
   error_check(i, "server listen");
   printf("[server] socket in listen state\n");
 
-  //free the structs used by getaddrinfo
+  // free the structs used by getaddrinfo
   free(hints);
   freeaddrinfo(results);
   return sd;
@@ -62,21 +63,21 @@ int server_connect (int sd) {
 int client_setup (char * server) {
   int sd, i;
 
-  //create the socket
+  // create the socket
   sd = socket(AF_INET, SOCK_STREAM, 0);
   error_check(sd, "client socket");
 
-  //run getaddrinfo
-  /* hints->ai_flags not needed because the client
-     specifies the desired address. */
+  // run getaddrinfo
+  // hints->ai_flags not needed because the client
+  // specifies the desired address.
   struct addrinfo * hints, * results;
   hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET;  //IPv4
-  hints->ai_socktype = SOCK_STREAM;  //TCP socket
+  hints->ai_family = AF_INET;  // IPv4
+  hints->ai_socktype = SOCK_STREAM;  // TCP socket
   getaddrinfo(server, PORT, hints, &results);
 
-  //connect to the server
-  //connect will bind the socket for us
+  // connect to the server
+  // connect will bind the socket for us
   i = connect(sd, results->ai_addr, results->ai_addrlen);
   error_check(i, "client connect");
 
